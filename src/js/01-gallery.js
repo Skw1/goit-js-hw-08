c8f54a76107947affe75ae9e6710ed7547a2614f
Andrii Doluda <andrewdoluda@gmail.com>
2023-12-18 15:26:37 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+container.insertAdjacentHTML('beforeend', galleryList(galleryItems));
+
+function galleryList(arr) {
+  return arr.map(({ preview, original, description }) =>
+        `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}">
+                </a>
+            </li>
+        `
+    )
+        .join('');
+}
+
+let bigGallery = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  
+});
